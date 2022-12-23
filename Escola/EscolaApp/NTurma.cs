fdf1667f526a4811671a51a6119687e72df6bfd8
Lielly Nátally <lielly.natally@escolar.ifrn.edu.br>
2022-12-23 17:51:27 -0300

    Escola.app

---
+++ Escola/EscolaApp/NTurma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+    class NTurma
+    {
+        private static List<Turma> turmas = new List<Turma>();
+        public static void Inserir(Turma t)
+        {
+            turmas.Add(t);
+        }
+        public static List<Turma> Listar()
+        {
+            return turmas;
+        }
+    }
+}
